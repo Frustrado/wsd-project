831cebd74fa03ff55005a2218ae1ea6439838c35
Marcel Jezierski <jezierskimarcel@gmail.com>
2019-12-11 23:38:40 +0100

    fix assigner basic

--- src/Assigner/AssignerAgent.java
+++ src/Assigner/AssignerAgent.java
@@ -21,18 +21,12 @@ public class AssignerAgent extends Agent {
             MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
             ACLMessage msg = myAgent.receive(mt);
             if (msg != null) {
+                ACLMessage reply = msg.createReply();
                 Integer x =10;
                 //Integer y =20;
-                if (price != null) {
                     //pozycja istnieje w katalogu, zwroc cene jako oferte
                     reply.setPerformative(ACLMessage.PROPOSE);
                     reply.setContent(String.valueOf(x.intValue()));
-                }
-                else {
-                    //pozycji nie ma w katalogu
-                    reply.setPerformative(ACLMessage.REFUSE);
-                    reply.setContent("not-available");
-                }
                 myAgent.send(reply);
             }
             else {
