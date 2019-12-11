40283f8c918659a986f4cbf38b047f07c4b57e03
Marcel Jezierski <jezierskimarcel@gmail.com>
2019-12-12 00:25:46 +0100

    my branch commit

--- src/Assigner/AssignerAgent.java
+++ src/Assigner/AssignerAgent.java
@@ -1,17 +1,40 @@
 package Assigner;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 
 public class AssignerAgent extends Agent {
     protected void setup() {
-        System.out.println("Agent auto "+getAID().getName()+" zaczal dzialanie.");
+        System.out.println("AssignerAgent "+getAID().getName()+" started.");
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("assigning");
+        sd.setName("JADE-assigning");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        addBehaviour(new AssignTraceServer());
     }
 
     protected void takeDown() {
         //myGui.dispose();
+        try {
+            DFService.deregister(this);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
         System.out.println("Agent auto "+getAID().getName()+" zakonczyl.");
     }
 
@@ -21,6 +44,8 @@ public class AssignerAgent extends Agent {
             MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
             ACLMessage msg = myAgent.receive(mt);
             if (msg != null) {
+                String title = msg.getContent();
+                System.out.println("Message from agent auto" + title);
                 ACLMessage reply = msg.createReply();
                 Integer x =10;
                 //Integer y =20;
--- src/Car/CarAgent.java
+++ src/Car/CarAgent.java
@@ -1,16 +1,90 @@
 package Car;
+import jade.core.AID;
 import jade.core.Agent;
-
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 
 public class CarAgent extends Agent{
+    private AID assigner;
     protected void setup() {
         System.out.println("Agent auto "+getAID().getName()+" zaczal dzialanie.");
+        addBehaviour(new TickerBehaviour(this, 20000)
+        {
+            protected void onTick()
+            {
+                //szukaj tylko jesli zlecony zostal tytul pozycji
+                //aktualizuj liste znanych sprzedawcow
+                DFAgentDescription template = new DFAgentDescription();
+                ServiceDescription sd = new ServiceDescription();
+                sd.setType("book-selling");
+                template.addServices(sd);
+                try
+                {
+                    DFAgentDescription[] result = DFService.search(myAgent, template);
+                    System.out.println("Znaleziono sprzedajacych:");
+                    assigner = new AID();
+                    assigner = result[0].getName();
+                }
+                catch (FIPAException fe)
+                {
+                    fe.printStackTrace();
+                }
+
+                myAgent.addBehaviour(new RequestPerformer());
+
+            }
+        });
     }
 
     protected void takeDown() {
         //myGui.dispose();
         System.out.println("Agent auto "+getAID().getName()+" zakonczyl.");
+
+    }
+
+    private class RequestPerformer extends Behaviour {
+        private MessageTemplate mt;
+        Integer answer;
+        private int step = 0;
+        public void action() {
+            switch (step) {
+                case 0:
+                    //call for proposal (cfp) do znalezionych sprzedajacych
+                    ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+                    cfp.addReceiver(assigner);
+                    cfp.setContent("KAPPA");
+                    cfp.setConversationId("assigning");
+                    cfp.setReplyWith("cfp" + System.currentTimeMillis()); //unikalna wartosc
+                    myAgent.send(cfp);
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("assigning"),
+                            MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+                    step = 1;
+                    break;
+                case 1:
+                    //odbior ofert od sprzedajacych
+                    ACLMessage reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        if (reply.getPerformative() == ACLMessage.PROPOSE) {
+                            //otrzymano oferte
+                            answer = Integer.parseInt(reply.getContent());
+                            System.out.println("Odpowiedz: " + answer);
+                        }
+                    } else {
+                        block();
+                    }
+                    break;
+            }
+        }
+        public boolean done() {
+            return (answer==10);
+        }
     }
 
 
