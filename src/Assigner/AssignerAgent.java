6acd0d67f0da2d1ab901ef730c7f6a77e6d9e1cd
Marcel Jezierski <jezierskimarcel@gmail.com>
2019-12-11 23:36:28 +0100

    basic assigner

--- src/Assigner/AssignerAgent.java
+++ src/Assigner/AssignerAgent.java
@@ -1,6 +1,8 @@
 package Assigner;
 import jade.core.Agent;
-
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 
 public class AssignerAgent extends Agent {
@@ -13,5 +15,29 @@ public class AssignerAgent extends Agent {
         System.out.println("Agent auto "+getAID().getName()+" zakonczyl.");
     }
 
-
+    private class AssignTraceServer extends CyclicBehaviour {
+        public void action() {
+            //tylko oferty
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {
+                Integer x =10;
+                //Integer y =20;
+                if (price != null) {
+                    //pozycja istnieje w katalogu, zwroc cene jako oferte
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(x.intValue()));
+                }
+                else {
+                    //pozycji nie ma w katalogu
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            }
+            else {
+                block();
+            }
+        }
+    }
 }
