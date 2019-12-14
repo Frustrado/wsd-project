dd0c69e580797e59bec4230dd5111f06a858c14a
Paweł Zych <pawel123fcb@gmail.com>
2019-12-14 15:58:14 +0100

    connector class base

--- src/agents/assigner/dto/DBConnector.java
+++ src/agents/assigner/dto/DBConnector.java
@@ -3,7 +3,10 @@ package agents.assigner.dto;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DBConnector {
 
@@ -28,6 +31,19 @@ public class DBConnector {
         return conn;
     }
 
+    public ArrayList<ParkingState> getAllParkings{
+        String query = "select d.park_id, d.demand, p.name, p.xposition, p.yposition, p.max_places, p.places_taken from demand_parkings d join parkings p on d.park_id = p.id";
+
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ArrayList<ParkingState> result;
+
+    }
+    private ResultSet executeSelectQuery(string queryString, )
     public void test(){
         String SQL_QUERY = "insert into creators values (9,'Janek','Olga', 33)";
         try {
