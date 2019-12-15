bc93a685c3e4f6033bc2698948f29e13f5ac90f5
Marcel Jezierski <jezierskimarcel@gmail.com>
2019-12-15 18:03:44 +0100

    comments added

--- src/agents/decision/dto/Proposition.java
+++ src/agents/decision/dto/Proposition.java
@@ -6,7 +6,9 @@ import agents.car.dto.GPSPos;
 import java.io.Serializable;
 
 public class Proposition implements Serializable {
+    //car current position
     public GPSPos carPos;
+    //proposed parking
     public ParkingState parking;
 
     public Proposition(GPSPos carPosition, ParkingState proposedParking) {
